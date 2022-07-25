915d0b4d33c4af361732975fec4c0262cba14b58
AlexisLoya <alexloy117@gmail.com>
2022-07-25 10:17:02 -0500

    Add firebase connection

--- mobile/src/main/java/com/utez/edu/mx/activitysix/MainActivity.kt
+++ mobile/src/main/java/com/utez/edu/mx/activitysix/MainActivity.kt
@@ -3,27 +3,32 @@ package com.utez.edu.mx.activitysix
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.utez.edu.mx.activitysix.databinding.ActivityMainBinding
 
 const val EXTRA_MESSAGE = "com.utez.edu.mx.activitysix.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
-
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        db = Firebase.firestore
+        binding.addCollection.setOnClickListener {
+            val editText = "Hello"
+            val message = editText
+            val intent = Intent(this, MainActivityFormCollection::class.java).apply {
+                putExtra(EXTRA_MESSAGE, message)
+            }
+            startActivity(intent)
 
-        //val button:Button = findViewById(R.id.addCollection)
-    }
-
-    fun addCollection(view: View) {
-        val editText = "Hello"
-        val message = editText
-        val intent = Intent(this, MainActivityFormCollection::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
         }
-        startActivity(intent)
     }
 }
--- mobile/src/main/java/com/utez/edu/mx/activitysix/MainActivityFormCollection.kt
+++ mobile/src/main/java/com/utez/edu/mx/activitysix/MainActivityFormCollection.kt
@@ -2,15 +2,50 @@ package com.utez.edu.mx.activitysix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.ktx.firestore
+import com.utez.edu.mx.activitysix.databinding.ActivityMainBinding
+import com.utez.edu.mx.activitysix.databinding.ActivityMainFormCollectionBinding
+import java.util.*
 
 class MainActivityFormCollection : AppCompatActivity() {
+    companion object{
+        const val  TAG = "firebase"
+    }
+    private lateinit var binding: ActivityMainFormCollectionBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_form_collection)
-        val uploadBtn:Button
-        val selectBtn:Button
-//        val imgref: DatabaseR
+        binding = ActivityMainFormCollectionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val nameText:TextView = binding.editName
+        val submitBtn:Button = binding.submitBtn
+        val selectBtn:Button = binding.selectBtn
+        val db = Firebase.firestore
+        val collection = hashMapOf(
+            "collection" to nameText.text.toString().lowercase(Locale.ROOT),
+            "title" to nameText.text.toString(),
+            "description" to 1815
+        )
+
+        submitBtn.setOnClickListener {
+            // Create a new user with a first and last name
+            db.collection("options")
+                .add(collection)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(
+                        MainActivityFormCollection.TAG,
+                        "DocumentSnapshot added with ID: ${documentReference.id}"
+                    )
+                }
+                .addOnFailureListener { e ->
+                    Log.w(MainActivityFormCollection.TAG, "Error adding document", e)
+                }
+        }
     }
 }
